2f32b02e2ad03665bc1872a349b3714ef0fb303b
shkim <sh.kim@tilon.com>
2024-02-08 08:14:59 +0900

    필터 추가 및 테스트

---
+++ src/main/java/com/cos/jwt/filter/MyFilter1.java
@@ -0,0 +1,20 @@
+package com.cos.jwt.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyFilter1 implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        System.out.println("필터 1");
+        chain.doFilter(request,response); // 계속 프로세스를 진행해야 함.
+
+        //이런식으로하면 필터가 hi를 출력하고 끝남.
+        //PrintWriter out = response.getWriter();
+        //out.print("hi");
+
+
+    }
+}
---
+++ src/main/java/com/cos/jwt/filter/MyFilter2.java
@@ -0,0 +1,19 @@
+package com.cos.jwt.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class MyFilter2 implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        System.out.println("필터 2");
+        chain.doFilter(request,response); // 계속 프로세스를 진행해야 함.
+
+        //이런식으로하면 필터가 hi를 출력하고 끝남.
+        //PrintWriter out = response.getWriter();
+        //out.print("hi");
+
+
+    }
+}
